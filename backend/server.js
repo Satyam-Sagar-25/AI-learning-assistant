1aa8246cd7676ae9bf42fd498d1d8a28daab2bd6
Satyam Sagar <satyamsagar54321@gmail.com>
2026-01-19 21:59:07 +0530

    geminiService done

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ import errorHandler from "./middleware/errorHandler.js"
 import authRoutes from "./routes/authRoutes.js"
 import documentRoutes from "./routes/documentRoutes.js"
 import flashcardRoutes from "./routes/flashcardRoutes.js"
+import aiRoutes from "./routes/aiRoutes.js"
 
 //*ES6 module_dirname alternative
 const __filename = fileURLToPath(import.meta.url);
@@ -48,6 +49,7 @@ app.use("/uploads",express.static(path.join(__dirname,"uploads")));//static file
 app.use("/api/auth",authRoutes);
 app.use("/api/documents",documentRoutes);
 app.use("/api/flashcards",flashcardRoutes);
+app.use("/api/aiRoutes",aiRoutes);
 
 
 
