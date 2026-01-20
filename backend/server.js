b2ae45489785c60c0818b8630f0eec17dac9c41d
Satyam Sagar <satyamsagar54321@gmail.com>
2026-01-20 17:56:22 +0530

    backend completed

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ import documentRoutes from "./routes/documentRoutes.js"
 import flashcardRoutes from "./routes/flashcardRoutes.js"
 import aiRoutes from "./routes/aiRoutes.js"
 import quizRoutes from "./routes/quizRoutes.js"
+import progressRoutes from "./routes/progressRoutes.js"
 
 //*ES6 module_dirname alternative
 const __filename = fileURLToPath(import.meta.url);
@@ -52,6 +53,7 @@ app.use("/api/documents",documentRoutes);
 app.use("/api/flashcards",flashcardRoutes);
 app.use("/api/ai",aiRoutes);
 app.use("/api/quizzes",quizRoutes);
+app.use("/api/progress",progressRoutes);
 
 
 
